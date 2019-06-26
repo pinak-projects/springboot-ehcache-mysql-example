702170bae2950f3c8421306bbc7a1a527ad7a566
pinakjakhar <pinak@thelattice.in>
2019-06-26 12:35:12 +0530

    added data.sql for database population with customers

---
+++ bin/main/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `customer_details` (`customer_id`, `created_on`, `customer_name`, `customer_password`, `customer_phone`, `updated_on`)
+	   VALUES ('1', CURRENT_TIMESTAMP, 'Aaden Ferrell', 'ar88pXkdj8', '9876543210', CURRENT_TIMESTAMP),
+			  ('2', CURRENT_TIMESTAMP, 'Ross Cox', 'kGmB9UrHts', '9876543211', CURRENT_TIMESTAMP),
+			  ('3', CURRENT_TIMESTAMP, 'Gerardo Fox', 'fHKNuwLooh', '9876543212', CURRENT_TIMESTAMP),
+			  ('4', CURRENT_TIMESTAMP, 'Davian Patrick', 'q5co8uN7mf', '9876543213', CURRENT_TIMESTAMP),
+			  ('5', CURRENT_TIMESTAMP, 'Kade Mcdowell', 'is2dPKqgI0', '9876543214', CURRENT_TIMESTAMP),
+			  ('6', CURRENT_TIMESTAMP, 'Adison Ewing', 'eruXQ8uDU8', '9876543215', CURRENT_TIMESTAMP),
+			  ('7', CURRENT_TIMESTAMP, 'Melvin Good', 'LLrkCMbj58', '9876543216', CURRENT_TIMESTAMP),
+			  ('8', CURRENT_TIMESTAMP, 'Jaydan Blevins', 'T28Jg7v4l4', '9876543217', CURRENT_TIMESTAMP),
+			  ('9', CURRENT_TIMESTAMP, 'Cristina Clements', 'qwQgDQTuFS', '9876543218', CURRENT_TIMESTAMP),
+			  ('10', CURRENT_TIMESTAMP, 'Cheyenne Booker', 'TQSdjl9KAE', '9876543219', CURRENT_TIMESTAMP);
+					
